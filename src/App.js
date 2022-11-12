1c9cdfe021a580ac9ab26e12dcd12de8372ec0a6
Aji T A <ajiajith001@gmail.com>
2022-11-12 21:06:49 +0530

    useRef initial changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	/**
+	 * 1. DOM reference
+	 * 2. useRef for storing the previous state
+	 * 3. hold mutable value prevent re-render of component.
+	 */
+
+	const [name, setName] = useState("");
+	const resetInput = () => {
+		setName("");
+	};
+	return (
+		<div className="App">
+			<div>
+				<input
+					type="text"
+					name="name"
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+				/>
+				<button onClick={resetInput}>Reset</button>
+			</div>
+			<div>My name is {name}</div>
+		</div>
+	);
 }
 
 export default App;
